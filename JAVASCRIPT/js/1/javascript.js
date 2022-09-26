4ee2733762eecc1ca2e5a3064b6b9f787a2846d6
rdwnld <113399817+rdwnld@users.noreply.github.com>
2022-09-26 11:43:00 +0700

    Add files via upload

---
+++ JAVASCRIPT/js/1/javascript.js
@@ -0,0 +1,19 @@
+// var nama = "Rudio";
+// var usia = 16;
+
+// document.write("Nama Saya : " + nama);
+// document.write("<br> Usia Saya : " + usia);
+
+// alert("Selamat Datang Di Website Kami")
+
+
+var ytb = confirm("Youtube Kidyoo");
+
+if (ytb) {
+    window.location = "https://www.youtube.com/kidyoo";
+}
+
+
+else {
+    document.write("Disini aja kah?")
+}
